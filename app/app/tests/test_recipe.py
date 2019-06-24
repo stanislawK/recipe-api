cd4f66a6191f07eee89759e3ec57aee134ddfc9d
stanislawK <stanislaw.krupienko@gmail.com>
2019-06-24 12:27:56 +0200

    Added recipe detail view

--- app/app/tests/test_recipe.py
+++ app/app/tests/test_recipe.py
@@ -4,11 +4,16 @@ from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """Return recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def sample_recipe(user, **params):
@@ -21,6 +26,15 @@ def sample_recipe(user, **params):
     defaults.update(params)
 
     return Recipe.objects.create(user=user, **defaults)
+
+
+def sample_tag(user, name='Main course'):
+    """Create and return a sample tag"""
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Cinamon'):
+    return Ingredient.objects.create(user=user, name=name)
 
 
 class TestPublicRecipeApi():
@@ -72,4 +86,16 @@ class TestPrivateRecipeApi():
 
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 1
+        assert response.data == serializer.data
+
+    def test_view_recipe_detail(sefl, logged_client, registred_user):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=registred_user)
+        recipe.tags.add(sample_tag(user=registred_user))
+        recipe.ingredients.add(sample_ingredient(user=registred_user))
+
+        url = detail_url(recipe.id)
+        response = logged_client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
         assert response.data == serializer.data
