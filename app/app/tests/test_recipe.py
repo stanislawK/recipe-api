4c2b6f51fc9c73789072a317640d137423319024
stanislawK <stanislaw.krupienko@gmail.com>
2019-06-25 15:58:28 +0200

    Added filter by tags and ingredients feature

--- app/app/tests/test_recipe.py
+++ app/app/tests/test_recipe.py
@@ -235,3 +235,56 @@ class TestRecipeImageUpload():
             format='multipart'
         )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_filter_recipes_by_tags(self, logged_client, registred_user):
+        """Test returning recipes with specific tags"""
+        recipe1 = sample_recipe(user=registred_user, title='Thai vege curry')
+        recipe2 = sample_recipe(
+            user=registred_user,
+            title='Aubergine with tahini'
+        )
+        tag1 = sample_tag(user=registred_user, name='Vegan')
+        tag2 = sample_tag(user=registred_user, name='Vegetarian')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = sample_recipe(user=registred_user, title='Fish and chips')
+
+        response = logged_client.get(
+            RECIPES_URL,
+            {'tags': f'{tag1.id}, {tag2.id}'}
+        )
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+        assert len(response.data) == 2
+        assert serializer1.data in response.data
+        assert serializer2.data in response.data
+        assert serializer3.data not in response.data
+
+    def test_filter_recipes_by_ingredients(
+        self,
+        logged_client,
+        registred_user
+    ):
+        """Test returning recipes with specific ingredients"""
+        recipe1 = sample_recipe(user=registred_user, title='Beans tost')
+        recipe2 = sample_recipe(user=registred_user, title='Chicken')
+        ingredient1 = sample_ingredient(user=registred_user, name='beans')
+        ingredient2 = sample_ingredient(user=registred_user, name='soy')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+        recipe3 = sample_recipe(user=registred_user, title='mushrooms')
+
+        response = logged_client.get(
+            RECIPES_URL,
+            {'ingredients': f'{ingredient1.id},{ingredient2.id}'}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        assert len(response.data) == 2
+        assert serializer1.data in response.data
+        assert serializer2.data in response.data
+        assert serializer3.data not in response.data
